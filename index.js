443bea23eed32309aaaacca20e667b381530c9ac
toan.nguyenba <batoan2898@gmail.com>
2021-09-16 00:19:54 +0700

    init

---
+++ index.js
@@ -0,0 +1,32 @@
+var express = require('express')
+var app = express()
+
+var todos =[{
+  id: 1,
+  description: 'Build a simple API - nodejs',
+  completed: false
+ }, {
+  id: 2,
+  description: 'Go to T-beer - team building',
+  completed: false
+ }, {
+  id: 3,
+  description: 'Feed the dog ',
+  completed: true
+ }];
+
+var requestTime = function (req, res, next){
+  req.requestTime = Date.now()
+  next()
+}
+
+app.use(requestTime)
+
+app.get('/list',function (req, res){
+  res.json(todos)
+})
+
+app.listen(3000)
+
+
+module.exports = app;

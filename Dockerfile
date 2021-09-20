61682b1391366ad0f364b51e65b3f667d7d13386
toan.nguyenba <batoan2898@gmail.com>
2021-09-20 13:50:21 +0700

    add .env

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,4 @@ FROM node:13-apline
 
 WORKDIR /home/node/server
 
-EXPOSE 80
-
 CMD [ "node", "index.js" ]
--- index.js
+++ index.js
@@ -1,20 +1,16 @@
 var express = require('express')
+const dotenv = require('dotenv');
+
+dotenv.config()
+
 var app = express()
 
 var todos = {"code":200,"data":{"items":[{"plant":"1201","sloc":"1001","sloc_name":"Wood","plant_name":"AA Long An"},{"plant":"1201","sloc":"1011","sloc_name":"Core/EBd/Lam.","plant_name":"AA Long An"},{"plant":"1201","sloc":"1012","sloc_name":"Finished Sheets","plant_name":"AA Long An"},{"plant":"1201","sloc":"1031","sloc_name":"Hardwares","plant_name":"AA Long An"},{"plant":"1201","sloc":"1041","sloc_name":"Metalworks","plant_name":"AA Long An"},{"plant":"1201","sloc":"1051","sloc_name":"Paint/Coatings","plant_name":"AA Long An"},{"plant":"1201","sloc":"1061","sloc_name":"Fabric","plant_name":"AA Long An"},{"plant":"1201","sloc":"1071","sloc_name":"Leather","plant_name":"AA Long An"},{"plant":"1201","sloc":"1081","sloc_name":"Uphol. foam","plant_name":"AA Long An"},{"plant":"1201","sloc":"1091","sloc_name":"Furn. glass","plant_name":"AA Long An"}],"meta":{"total":51,"page":1}}}
 
-var requestTime = function (req, res, next){
-  req.requestTime = Date.now()
-  next()
-}
-
-app.use(requestTime)
-
 app.get('/list',function (req, res){
   res.json(todos)
+})
 
-
-app.listen(80)
-
+app.listen(process.env.NODE_DOCKER_PORT)
 
 module.exports = app;
